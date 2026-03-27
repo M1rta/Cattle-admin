13d42da844b0185d4da4377eebe24c792ad4fc7b
M1rta <westeban.elizondo999@gmail.com>
2026-03-27 15:37:55 -0600

    Update de index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template
 from flask_cors import CORS
-from routes.ganado_routes import ganado_bp
-from routes.auth.auth_routes import auth_bp
-
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 CORS(app)
-
-app.register_blueprint(ganado_bp)
-app.register_blueprint(auth_bp)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# 👇 ESTO ES CLAVE PARA VERCEL
+def handler(request):
+    return app(request)
