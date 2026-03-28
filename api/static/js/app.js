afdbb05be6714684defe48bc15504308629ae686
M1rta <westeban.elizondo999@gmail.com>
2026-03-27 18:43:20 -0600

    update

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -9,7 +9,7 @@ if (!token) window.location.href = "/login";
 /* =========================
    API
    ========================= */
-const API = "http://127.0.0.1:5000";
+const API = "";
 
 function headersAuth(extraHeaders = {}) {
   return {
