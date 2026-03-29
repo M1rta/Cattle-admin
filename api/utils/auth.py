ef4d16755f52a712efc022ec36c6e91b81210d6b
M1rta <westeban.elizondo999@gmail.com>
2026-03-28 21:46:37 -0600

    a ver

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -4,7 +4,10 @@ def get_user_id_from_auth():
     auth = request.headers.get("Authorization", "")
     if not auth.startswith("Bearer "):
         return None
+        
     token = auth.replace("Bearer ", "").strip()
-    if not token.isdigit():
+    
+    if not token:
         return None
-    return int(token)
+        
+    return token
